6da0e072c0c920e132d9333ab79673a273529082
CarolynnFleming <cfcatpink@gmail.com>
2022-01-05 15:26:05 -0800

    start

--- fetch-utils.js
+++ fetch-utils.js
@@ -5,11 +5,17 @@ const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function createTodo(todo){
     // create a single incomplete todo with the correct 'todo' property for this user in supabase
+    const response = await client
+        .from('todos')
+        .insert([{ todo }]);
 
     return checkError(response);
 }
 
 export async function deleteAllTodos() {
+    const response = await client
+        .from('todos')
+        .delete();
     // delete all todos for this user in supabase
 
     return checkError(response);
@@ -17,12 +23,19 @@ export async function deleteAllTodos() {
 
 export async function getTodos() {
     // get all todos for this user from supabase
+    const response = await client 
+        .fom('todos')
+        .select();
 
     return checkError(response);    
 }
 
 export async function completeTodo(id) {
     // find the and update (set complete to true), the todo that matches the correct id
+    const response = client 
+    .from('todos')
+    .update({ complete: true })
+    .match({ id: id });
 
     return checkError(response);    
 }
