1cd0b283f4793ddfef2791d4d235a9971e3d57fa
CarolynnFleming <cfcatpink@gmail.com>
2022-01-05 16:14:54 -0800

    more

--- render-utils.js
+++ render-utils.js
@@ -1,12 +1,23 @@
 export function renderTodo(todo) {
     // create a div and a p tag
+    
+    const todoItem = document.createElement('div');
+    const todoEl = document.createElement('p');
+
     // depending on whether the todo is complete, give the div the appropriate css class ('complete' or 'incomplete')
-
+    if (todo.complete){
+        todoItem.classList.add('complete');
+    } else {
+        todoItem.classList.add('not-complete');    
+    }
     // add the 'todo' css class no matter what
-
+    todoItem.textContent = todo;
     // put the todo's text into the p tag
-
+    todoEl.textContent = `${todo.todo}`;
     // append stuff
-
+    todoItem.append(todoEl);
+    todoEl.append(todo)
     // return the div
+    return todoItem;
+    
 }
